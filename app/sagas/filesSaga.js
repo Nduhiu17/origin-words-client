49b6fced018d6428fe8ef2496ccc7a05a75282fc
Nduhiu17 <nduhiu254@gmail.com>
2020-08-19 21:30:30 +0300

    [CHORE]Display App messages - create snack bar component - create snack bar reducer - dispatch actions at saga level on error

--- app/sagas/filesSaga.js
+++ app/sagas/filesSaga.js
@@ -1,15 +1,29 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
 import { takeEvery, put, call } from 'redux-saga/effects'
 import { FILES } from '../constants'
 import { fetchFiles } from '../api'
 import { setError, setFiles } from '../actions'
+import { setSnackbar } from "../reducers/snackbarReducer";
 
-export function* handleFilesLoad() {
+
+// const dispatch = useDispatch()
+//   console.log('====================================');
+//   console.log(dispatch);
+//   console.log('====================================');
+
+function* handleFilesLoad() {
+// const dispatch = useDispatch()
+
+
+
   try {
     const files = yield call(fetchFiles)
     yield put(setFiles(files))
   } catch (error) {
     // dispatch error
     yield put(setError(error.toString()))
+    yield put(setSnackbar(true, 'error', error.toString()))
   }
 }
 
