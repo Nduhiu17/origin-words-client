195d4f32f6f9f1dc7a3f8caacc9e066d12178100
Nduhiu17 <nduhiu254@gmail.com>
2020-08-20 11:11:12 +0300

    [FEATURE]Get all subcategories - create api service to fetch all the subcategories - create redux action and reducers to dispatch fetch subcategories - create saga to handle subcategories fetch actions

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -11,6 +11,7 @@ import loadingReducer from './loadingReducer'
 import errorReducer from './errorReducer'
 import snackbarReducer from './snackbarReducer'
 import categoriesReducer from './categoriesReducer'
+import subcategoriesReducer from './subcategoriesReducer'
 
 /**
  * Merges the main reducer with the router state and dynamically injected reducers
@@ -23,6 +24,7 @@ export default function createReducer(injectedReducers = {}) {
     error: errorReducer,
     snackbar: snackbarReducer,
     categories: categoriesReducer,
+    subcategories: subcategoriesReducer,
 
     ...injectedReducers,
   })
